fbe9f2480f496ec330b7cad0b510bc7f44a13fa1
bshaydullin <bshaydullin@gmail.com>
2021-07-18 21:29:19 +0300

    added first autotest

--- src/test/java/FirstTest/TextboxTests.java
+++ src/test/java/FirstTest/TextboxTests.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class TextboxTest {
+public class TextboxTests {
 
     @BeforeAll
     static void setup() {
